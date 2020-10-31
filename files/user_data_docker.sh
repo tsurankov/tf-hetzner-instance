76d1b7302b1e0b076590ae2f1dda08c920b08fd2
Sergey Tsurankov <sergeytsurankov@gmail.com>
2020-10-31 22:51:45 +0300

    Added docker installation

--- files/user_data_docker.sh
+++ files/user_data_docker.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
  apt-transport-https \
  ca-certificates \
  curl \
  gnupg2 \
  software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-get update
+apt install docker-ce -y
+systemctl enable docker
+systemctl start docker
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
